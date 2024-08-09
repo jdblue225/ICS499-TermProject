8778e59889ff1f9694eef57c9a650c1ba6074810
jdblue225 <63324815+jdblue225@users.noreply.github.com>
2024-08-09 17:49:43 -0500

    Added Space Invaders. Its broken.

--- gameArcade/src/main/java/module-info.java
+++ gameArcade/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.cookiecoders.gamearcade {
     requires java.compiler;
     requires java.desktop;
     requires org.controlsfx.controls;
+    requires javafx.media;
 
 
     opens com.cookiecoders.gamearcade to javafx.fxml;
@@ -13,4 +14,5 @@ module com.cookiecoders.gamearcade {
     opens com.cookiecoders.gamearcade.util to javafx.fxml;
     exports com.cookiecoders.gamearcade.ui.controllers;
     opens com.cookiecoders.gamearcade.ui.controllers to javafx.fxml;
+    exports com.cookiecoders.gamearcade.games;
 }
